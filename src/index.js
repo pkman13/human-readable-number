f34beaa82e952b30172d553677dd508c01d80ea1
Sergey <sergey.grinkevich@gmail.com>
2022-08-10 23:28:39 +0300

    feat: function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function toReadable (number) {
-  
+    const unit = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const ten = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const digit = number%10;
+    const integers = Math.floor(number/10);
+
+    if (number < 20) {
+        return unit[number];
+    }
+
+    if (number < 100) {
+        
+        return ten[integers] + (digit == 0 ? '' : ' ' + unit[digit]);
+    }         
+
+    if (number < 1000) {
+        return unit[Math.floor(number/100)] + ' hundred' + (number%100 == 0 ? '' : ' ' + toReadable(number%100)); //+ (digit === 0 ? '' : ' ' + unit[digit]);
+    }
+
+    
+
+
 }
+
+
+
